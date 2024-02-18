8c4eb330e1e1f48177e49a7f927e2b87f742959e
Gobykrishanth <159746741+Gobykrishanth@users.noreply.github.com>
2024-02-18 23:05:49 +0530

    Add files via upload

---
+++ Employee/src/main/java/com/example/Employee/repositories/EmployeeRepository.java
@@ -0,0 +1,12 @@
+package com.example.Employee.repositories;
+
+import com.example.Employee.entities.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+ 
+}
